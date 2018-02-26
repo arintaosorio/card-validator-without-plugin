e3905d785377bce3c1708e51ebf82b7b09ec44cb
arintaosorio <arintaosorio@gmail.com>
2018-02-26 01:42:57 -0600

    subiendo entero

--- js/app.js
+++ js/app.js
@@ -1,92 +1,114 @@
 const form = document.querySelector("form");
+//console.log(form);
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  if (validateCardDetails(form)) {
-    console.log("datos válido... enviar...");
-  } else {
-    console.log("datos inválidos");
-  }
-});
-
-
-const onlyNumbers = e => {
-  const key = window.event ? e.which : e.keyCode;
-  if (key < 48 || key > 57) {
-    e.preventDefault();
-  }
+// FUNCIÓN  PARA VALIDAR CVV
+const validateCVV = number => {
+    let value = parseInt(number.value);
+    if (value !== "" && value < 1000 && value > 99 && typeof value === 'number') {
+        number.className = 'success';
+        return true;
+    } else {
+        number.className = 'error';
+        return false;
+    }
 }
+// FUNCIÓN PARA VALIDAR NOMBRE
+const validateName = string => {
+    let nameVal = string.value.toLowerCase();
+    //console.log(nameVal);
+    const array = nameVal.split(' ');
+    if (array !== "" && array.length > 1 && array.length < 3) {
+        string.className = 'success';
+        return true;
+        //console.log('yes');
+    } else {
+        string.className = 'error';
+        return false;
+        //console.log('no');
+    }
+}
+//FUNCIÓN PARA VALIDAR LA FECHA DE EXPIRACIÓN
+const validateExpiration = string => {
+    const dateVal = string.value;
+    if (dateVal !== "") {
+        string.className = 'success';
+        return true;
+    } else {
+        string.className = 'error';
+        return false;
+    }
+}
+// FUNCIÓN PARA VALIDAR TARJETA CON ALGORITO DE LUHN
+const validateCardNumber = input => {
+    let numValue = input.value;
+    let cardArray = numValue.split('');
+    //console.log(cardArray);
+    let backwardsNumber = cardArray.reverse();
+    //console.log(backwardsNumber);
+    let even = 0;
+    let odd = 0;
+    let evenSum = 0;
+    for (var i = 0; i < 16; i++) {
+        if (i % 2 !== 1) {
+            odd += parseInt(backwardsNumber[i]);
+            // console.log(backwardsNumber[i]);
+        }
+    }
+    //console.log(odd);
 
+    for (var j = 1; j < 16; j++) {
 
-const numberCardValidation = (number) => {
-
-  let cardNumber = number.value.split(""); //convertimos en array
-  let inverse = cardNumber.reverse();//87541... números al revés
-  let odd = 0;
-  let even = 0;
-  let evenPlus = 0;
-
-
-    for(let i = 0; i < 16; i++){ //aquí sacamos la suma de numeros impares
-      if(i % 2 !== 1){
-        odd = odd + parseInt(inverse[i]);
-      }
+        if (i % 2 === 0 && backwardsNumber[j] <= 4) {
+            even += parseInt(backwardsNumber[j] * 2);
+        } else if (j % 2 === 1 && backwardsNumber[j] == 5) {
+            evenSum += parseInt(1);
+        } else if (j % 2 === 1 && backwardsNumber[j] == 6) {
+            evenSum += parseInt(3);
+        } else if (j % 2 === 1 && backwardsNumber[j] == 7) {
+            evenSum += parseInt(5);
+        } else if (j % 2 === 1 && backwardsNumber[j] == 8) {
+            evenSum += parseInt(7);
+        } else if (j % 2 === 1 && backwardsNumber[j] == 9) {
+            evenSum += parseInt(9);
+        }
     }
 
-        for(let j = 1; j < 16; j++){
-          //sacamos el números pares menores a 4 y multiplicamos por 2
-          if (j % 2 === 1 && parseInt(inverse[j]) <= 4){
-            even = even + (parseInt(inverse[j])*2);
-            // de aquí en adelante reasiganamos los valores mayores del
-          } else if (j % 2 === 1 && inverse[j] == 5){
-             evenPlus = evenPlus + parseInt(1);
-          } else if (j % 2 === 1 && inverse[j] == 6){
-             evenPlus = evenPlus + parseInt(3);
-          } else if (j % 2 === 1 && inverse[j] == 7){
-             evenPlus = evenPlus + parseInt(5);
-          } else if (j % 2 === 1 && inverse[j] == 8){
-             evenPlus = evenPlus + parseInt(7);
-          } else if (j % 2 === 1 && inverse[j] == 9){
-             evenPlus = evenPlus + parseInt(9);
-          }
-        }
+    //console.log(even);
+    //console.log(evenSum);
+    let totalSum = odd + even + evenSum;
+    //console.log(totalSum// console.log('valid'););
 
-      //hacemos la de nuestros numeros pares, pares mayores e impares y comprobamos si son módulo de 10
-      let ckeckCard = even + odd + evenPlus;
-      if(ckeckCard % 10 === 0){
-        number.classList.add("sucess");
-      } else {
-         console.log("Your card is an invalid card");
-      }
+    if (totalSum % 10 === 0) {
+        input.className = 'success';
+        return true;
+    } else {
+        input.className = 'error';
+        return false;
+    }
 }
-
-    
-
-
-
-const cvvValidation = number => {
-  if(number.value == 1){
-    number.classList.add("sucess");
-    console.log("hola");
-   
-  }
-}
-
-
 
 const validateCardDetails = element => {
-  const arrayForm =  Array.from(element);
+    const array = Array.from(element);
+    //console.log(array);
+    const cardNumber = array[0];
+    //console.log(cardNumber);
+    const expireDate = array[1];
+    //console.log(expireDate);
+    let cvv = array[2];
+    //console.log(cvv);
+    const name = array[3];
+    //console.log(name);
+    validateCVV(cvv);
+    validateName(name);
+    validateExpiration(expireDate);
+    validateCardNumber(cardNumber);
+};
 
-  const numberCard = arrayForm[0];
-  const dueDateCard = arrayForm[1];
-  const cvvCard = arrayForm[2];
-  const nameCard = arrayForm[3];
-
-  cvvValidation(cvvCard);
-  numberCardValidation(numberCard);
-
-  numberCard.addEventListener("keypress", onlyNumbers, false);
-  cvvCard.addEventListener("keypress", onlyNumbers, false);
-}
-validateCardDetails(form)
-
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if (validateCardDetails(form)) {
+        console.log("datos válido... enviar...");
+    } else {
+        console.log("datos inválidos");
+    }
+});
